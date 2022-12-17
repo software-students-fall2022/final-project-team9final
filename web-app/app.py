7dbe4b707a050086b47a6f30d0f5337ffe3b8c11
Brandon Chen <bc2982@nyu.edu>
2022-12-17 03:44:02 -0500

    changed port to 8080

--- web-app/app.py
+++ web-app/app.py
@@ -341,5 +341,5 @@ def updateLike():
 
 if __name__=='__main__':
     #app.run(debug=True)
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(debug=True, host='0.0.0.0', port=port)
