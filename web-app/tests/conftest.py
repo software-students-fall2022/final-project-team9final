bc4f1d52f0dee26f61ebcbf12ed36925114aa0a3
Seok Tae Kim <stk319@nyu.edu>
2022-12-17 13:59:53 -0500

    debug test

--- web-app/tests/conftest.py
+++ web-app/tests/conftest.py
@@ -17,12 +17,12 @@ def app_with_database(flask_app):
 @pytest.fixture(scope='session')
 def app_with_data(app_with_database):
     hashed_password = generate_password_hash("test")
-    Database.insert_one('users', {"username": "test", 'firstName': "test", 'lastName': "test", "password": hashed_password})
+    Database.insert_one('users', {"username": "test", 'firstName': "test", 'lastName': "test", "password": hashed_password, "stories":[], "followers":[],"following":[]})
     yield app_with_database
 
 @pytest.fixture(scope='session')
 def app_with_book(app_with_data):
-    _id = Database.insert_one('books',{"title": "test", 'story': "test", 'shared' : False, 'liked' : []})
+    _id = Database.insert_one('books',{"title": "test", 'story': ["test"], 'shared' : False, 'liked' : []})
     Database.update('users',{"username": "test"}, {'$push' : {'stories' : ObjectId(_id.inserted_id)}})
     yield app_with_data, _id.inserted_id
 
